ce39d292fd6315dfa585a268aac6d9cff4a61b38
benjaminmille <benjamin.mille@hotmail.fr>
2016-05-21 15:12:32 +0200

    Chapitre 7: Jouons avec la page Catégorie
    (Correction du bug de redirection 404)

--- src/Ens/BenjaminBundle/Controller/JobController.php
+++ src/Ens/BenjaminBundle/Controller/JobController.php
@@ -27,6 +27,7 @@ class JobController extends Controller
         foreach($categories as $category)
         {
             $category->setActiveJobs($em->getRepository('EnsBenjaminBundle:Job')->getActiveJobs($category->getId(), $this->container->getParameter('max_jobs_on_homepage')));
+            $category->setMoreJobs($em->getRepository('EnsBenjaminBundle:Job')->countActiveJobs($category->getId()) - $this->container->getParameter('max_jobs_on_homepage'));
         }
 
         return $this->render('job/index.html.twig', array(
@@ -66,7 +67,7 @@ class JobController extends Controller
     {
         $em = $this->getDoctrine()->getEntityManager();
 
-        $job = $em->getRepository('EnsBenjaminBundle:Job')->getActiveJobs($id);
+        $job = $em->getRepository('EnsBenjaminBundle:Job')->getActiveJob($id);
 
         if (!$job) {
             throw $this->createNotFoundException('Unable to find Job entity.');
--- src/Ens/BenjaminBundle/Entity/Category.php
+++ src/Ens/BenjaminBundle/Entity/Category.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace Ens\BenjaminBundle\Entity;
+use Ens\BenjaminBundle\Utils\Jobeet;
 
 /**
  * Category
@@ -28,6 +29,8 @@ class Category
     private $category_affiliates;
 
     private $active_jobs;
+
+    private $more_jobs;
 
     /**
      * Constructor
@@ -153,5 +156,51 @@ class Category
     public function getActiveJobs()
     {
         return $this->active_jobs;
+    }
+
+    public function setMoreJobs($jobs)
+    {
+        $this->more_jobs = $jobs >=  0 ? $jobs : 0;
+    }
+
+    public function getMoreJobs()
+    {
+        return $this->more_jobs;
+    }
+    /**
+     * @var string
+     */
+    private $slug;
+
+
+    /**
+     * Set slug
+     *
+     * @param string $slug
+     *
+     * @return Category
+     */
+    public function setSlug($slug)
+    {
+        $this->slug = $slug;
+
+        return $this;
+    }
+
+    /**
+     * Get slug
+     *
+     * @return string
+     */
+    public function getSlug()
+    {
+        return $this->slug;
+    }
+    /**
+     * @ORM\PreUpdate
+     */
+    public function setSlugValue()
+    {
+        $this->slug = Jobeet::slugify($this->getName());
     }
 }
